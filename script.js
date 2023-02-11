2d6b86d59feff9df06d345ff7137689f5fddc857
Love <lovesoni607@gmail.com>
2023-02-11 18:06:18 +0530

    data tyep and type of operator

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let js = "amazing";
 console.log(40 + 8 + 37);
 console.log("love");
 
+//string data type
 let firstName = "love"
 let lastName = "soni";
 console.log(firstName + " " + lastName);
+console.log("test ")
+
+//boolean data type
+let jsIsFun = true;
+console.log(jsIsFun);
+
+// typeOf operator : to check the data type
+console.log(typeof jsIsFun);
