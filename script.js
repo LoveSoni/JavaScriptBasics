fe1e8eabda1b4812d16392e07e2515c9cff09fe9
Love <lovesoni607@gmail.com>
2023-02-11 18:15:07 +0530

    data tyep and type of operator

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ console.log(jsIsFun);
 
 // typeOf operator : to check the data type
 console.log(typeof jsIsFun);
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 23.34);
+console.log(typeof 'love');
+
+
+// undefined data type
+let undefVariable;
+console.log(undefVariable);
+// check the type
+console.log(typeof undefVariable);
+
+
+console.log(typeof null);
