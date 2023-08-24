9682e487b46ea27540bf4172a34629e700944669
Love <lovesoni607@gmail.com>
2023-08-24 16:55:51 +0530

    while_do_while loop

--- 05_iterations/02_whiledowhile.js
+++ 05_iterations/02_whiledowhile.js
@@ -1,1 +1,27 @@
 'use strict'
+
+// while loop
+
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+
+}
+
+// array with while loop
+const myHeroes = ['spiderman', 'thor', 'superman']
+
+let j = 0;
+while (j < myHeroes.length) {
+    console.log(myHeroes[j]);
+    j++;
+}
+
+
+// do loop
+let k = 1;
+do {
+    console.log(k);
+    k++;
+} while (k <= 10);
