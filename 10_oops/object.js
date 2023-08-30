5cfd44bc89066904d0f6670da4d47653686f5c87
Love <lovesoni607@gmail.com>
2023-08-30 22:05:57 +0530

    new keyword

--- 10_oops/object.js
+++ 10_oops/object.js
@@ -10,8 +10,26 @@ console.log(sum(1, 2));
 console.log(sum.num);
 console.log(sum.prototype);
 
-function createUser(firstname, lastname) {
-    this.firstname = firstname;
-    this.lastname = lastname;
 
+
+console.log('Adding our own method to function prototype');
+console.log('***********************************************')
+
+
+function createNewUser(fname, lname) {
+    this.fname = fname;
+    this.lname = lname;
 }
+
+createNewUser.prototype.getRandomPassword = function () {
+    this.password = parseInt(Math.random() * 100);
+    return this;
+}
+
+const user1 = new createNewUser('love', 'lovel');
+const user2 = new createNewUser('sam', 'ingwel');
+console.log(createNewUser.prototype);
+
+console.log(user1.getRandomPassword());
+
+
