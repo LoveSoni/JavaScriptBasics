a974c72a4fdf47c62cb903f1fa2fceb5e9d1ebec
Love <lovesoni607@gmail.com>
2023-09-01 22:19:54 +0530

    getter_setter class

---
+++ 10_oops/getterAndSetter.js
@@ -0,0 +1,10 @@
+'use strict'
+class Student {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+}
+
+const love = new Student('love', 'love.love@gmail.com');
+console.log(love);
