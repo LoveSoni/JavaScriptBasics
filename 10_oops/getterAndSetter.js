097476a11d29ad013a80dc2e4c12d2cbbcbe5dcf
Love <lovesoni607@gmail.com>
2023-09-02 13:20:41 +0530

    getter and setter

--- 10_oops/getterAndSetter.js
+++ 10_oops/getterAndSetter.js
@@ -4,7 +4,31 @@ class Student {
         this.name = name;
         this.email = email;
     }
+
+    // it will be automatically invoked
+    // as we already have name and email property defined in the constructor
+    // it would result in call stack size exceedd error
+    // so in our getter and setter we can define new property by usint this._
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(email) {
+        this._email = email;
+    }
+
+
 }
 
 const love = new Student('love', 'love.love@gmail.com');
-console.log(love);
+
+console.log(love.name);
+console.log(love.email);
